b40bdaa28fb83a8dfe7be05409ba505b22c88960
Huijie <91745551+metaphor987@users.noreply.github.com>
2022-05-08 16:55:57 -0400

    write a basic movie finder

---
+++ models/Course.js
@@ -0,0 +1,14 @@
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+const Mixed = Schema.Types.Mixed;
+
+var courseSchema = Schema( {
+    title: String,
+    year: Number,
+    cast: Mixed,
+    genres: Mixed,
+} );
+
+module.exports = mongoose.model( 'Course', courseSchema );
